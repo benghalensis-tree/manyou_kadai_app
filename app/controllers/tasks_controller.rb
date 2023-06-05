1a7271c368c664b3a56c6bf32bd9ea2dbeeb08ad
genki utsumi <zabanya0770@gmail.com>
2023-06-05 21:45:27 +0900

    入力フォームを作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,14 +4,22 @@ class TasksController < ApplicationController
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+    @task = Task.new(task_params)
+    @task.save
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:task_name, :content, :status, :priority, :end_date)
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,8 +1,8 @@
 class Task < ApplicationRecord
-  validates :task_name, pressence: true, length: { maximum: 30 }
-  validates :content, pressence: true, length: { maximum: 200 }
-  validates :status, pressence: true, length: { maximum: 20 }
-  validates :priority, pressence: true, length: { maximum: 3}
-  validates :end_date, pressence: true, length: { maximum: 10 }
+  validates :task_name, presence: true, length: { maximum: 30 }
+  validates :content, presence: true, length: { maximum: 200 }
+  validates :status, presence: true, length: { maximum: 20 }
+  validates :priority, presence: true, length: { maximum: 3}
+  validates :end_date, presence: true, length: { maximum: 10 }
 
 end
