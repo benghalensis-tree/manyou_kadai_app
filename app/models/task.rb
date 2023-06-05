d2c85ae144bb03623c11aba4530406798cf08c0f
genki utsumi <zabanya0770@gmail.com>
2023-06-05 21:16:01 +0900

    tasksコントローラを作成

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,8 @@
 class Task < ApplicationRecord
+  validates :task_name, pressence: true, length: { maximum: 30 }
+  validates :content, pressence: true, length: { maximum: 200 }
+  validates :status, pressence: true, length: { maximum: 20 }
+  validates :priority, pressence: true, length: { maximum: 3}
+  validates :end_date, pressence: true, length: { maximum: 10 }
+
 end
