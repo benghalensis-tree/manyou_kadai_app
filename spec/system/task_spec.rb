1da76a5d072a9e69b4b859547203e62b9a05404f
genki utsumi <zabanya0770@gmail.com>
2023-06-06 15:45:40 +0900

    ModelSpecの作成開始

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -23,11 +23,9 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '詳細表示機能' do
      context '任意のタスク詳細画面に遷移した場合' do
        it '該当タスクの内容が表示される' do
-
         content = FactoryBot.create(:task, content: 'hogehoge')
         visit task_path(content)
         expect(page).to have_content 'hogehoge'
-
        end
      end
   end
