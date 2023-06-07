f9fe491a51afcb9096514b26540a184e1b9cfc7c
genki utsumi <zabanya0770@gmail.com>
2023-06-07 22:08:34 +0900

    ソート機能のテストを実装

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -16,8 +16,9 @@ RSpec.describe 'タスク管理機能', type: :system do
   end
   describe '一覧表示機能' do
     before do
-      task = FactoryBot.create(:task, id:2, task_name: 'hoge')
-      task2 = FactoryBot.create(:task, id:1, task_name: 'fuga')
+      task = FactoryBot.create(:task, id:2, task_name: 'hoge', end_date: 20230607)
+      task2 = FactoryBot.create(:task, id:3, task_name: 'fuga', end_date: 20230604)
+      task3 = FactoryBot.create(:task, id:1, task_name: 'hogefuga', end_date: 20230606)
       visit tasks_path
     end
     context '一覧画面に遷移した場合' do
@@ -27,6 +28,12 @@ RSpec.describe 'タスク管理機能', type: :system do
     end
     context 'タスクが作成日時の降順に並んでいる場合' do
       it '新しいタスクが一番上に表示される' do
+        task_list = all('.task_row')
+        expect(task_list[0]).to have_content 'fuga'
+      end
+    end
+    context 'ソートボタンを押した場合' do
+      it '期日が近いものが一番上に表示される' do
         task_list = all('.task_row')
         expect(task_list[0]).to have_content 'fuga'
       end
