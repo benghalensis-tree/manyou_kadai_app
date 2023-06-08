666b813c01a7886cc909323762f5bdd5f0d02251
genki utsumi <zabanya0770@gmail.com>
2023-06-08 20:59:23 +0900

    優先度ソートのテストを実装

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'タスク管理機能', type: :system do
         fill_in 'task_name', with: 'hogehoge'  
         fill_in 'content', with: 'hogehoge'  
         select '未着手', from: 'ステータス'
-        fill_in 'priority', with: 3  
+        select '高', from: '優先度'
         fill_in 'end_date', with: 20230610  
         click_on '登録する'
         expect(page).to have_content 'hogehoge'
@@ -16,9 +16,9 @@ RSpec.describe 'タスク管理機能', type: :system do
   end
   describe '一覧表示機能' do
     before do
-      task = FactoryBot.create(:task, status:'未着手', id:2, task_name: 'hoge', end_date: 20230607)
-      task2 = FactoryBot.create(:task,status:'未着手', id:3, task_name: 'fuga', end_date: 20230604)
-      task3 = FactoryBot.create(:task, status:'未着手',id:1, task_name: 'hogefuga', end_date: 20230606)
+      task = FactoryBot.create(:task, status:'未着手', id:2, task_name: 'hoge', priority: '中', end_date: 20230607)
+      task2 = FactoryBot.create(:task,status:'未着手', id:3, task_name: 'fuga', priority: '高', end_date: 20230604)
+      task3 = FactoryBot.create(:task, status:'未着手',id:1, task_name: 'hogefuga', priority: '低', end_date: 20230606)
       visit tasks_path
     end
     context '一覧画面に遷移した場合' do
@@ -34,6 +34,12 @@ RSpec.describe 'タスク管理機能', type: :system do
     end
     context 'ソートボタンを押した場合' do
       it '期日が近いものが一番上に表示される' do
+        task_list = all('.task_row')
+        expect(task_list[0]).to have_content 'fuga'
+      end
+    end
+    context '優先度ボタンを押した場合' do
+      it '優先度の高いものが上に表示される' do
         task_list = all('.task_row')
         expect(task_list[0]).to have_content 'fuga'
       end
@@ -61,7 +67,6 @@ RSpec.describe 'タスク管理機能', type: :system do
       end
     end
   end
-
   describe '詳細表示機能' do
      context '任意のタスク詳細画面に遷移した場合' do
        it '該当タスクの内容が表示される' do
