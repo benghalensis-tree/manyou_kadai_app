46606b51d6800533d98d79ea109326137795a5bf
genki utsumi <zabanya0770@gmail.com>
2023-06-08 20:49:36 +0900

    モデルにenumを定義

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,6 +4,7 @@ class Task < ApplicationRecord
   validates :status, presence: true
   validates :priority, presence: true, length: { maximum: 3}
   enum status: {未着手:1, 着手中:2, 完了:3}
+  enum priority: {低:1, 中:2, 高:3}
 
   scope :search_status, -> (status_params){ where(status: status_params) }
   scope :search_task_name, -> (task_name_params){ where('task_name LIKE ?', "%#{task_name_params}%") }
