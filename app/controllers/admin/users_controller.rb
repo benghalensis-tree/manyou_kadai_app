660f6c85e176e782df977d4d2eab909bfbc8805d
genki utsumi <zabanya0770@gmail.com>
2023-06-10 20:21:42 +0900

    ユーザー一覧ページを作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,11 +1,21 @@
 class Admin::UsersController < ApplicationController
 
   def index
+    @users = User.all
   end
 
   def new
   end
 
   def create
+  end
+
+  def edit
+  end
+
+  def show
+  end
+  
+  def destroy
   end
 end
