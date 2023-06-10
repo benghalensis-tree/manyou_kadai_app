7f337963763332e8f7a96f93e3fab679b42b98f2
genki utsumi <zabanya0770@gmail.com>
2023-06-10 20:52:02 +0900

    user削除機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   before_validation { email.downcase! if email.present?}
   has_secure_password
   validates :password, length: { minimum: 6}
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
 end
