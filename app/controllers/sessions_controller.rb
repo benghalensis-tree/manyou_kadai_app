3139681df29df5212259299abf95d1552578a6fc
genki utsumi <zabanya0770@gmail.com>
2023-06-10 16:59:11 +0900

    ログアウト機能を実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,5 +15,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    session.delete(:user_id)
+    flash[:notice] = 'ログアウトしました'
+    redirect_to new_session_path
   end
 end
