9c82632bd8adb4f6bbccc515cd2167632babbeb3
genki utsumi <zabanya0770@gmail.com>
2023-06-10 18:40:01 +0900

    マイページタスク一覧機能実装完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,13 +17,41 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @user_tasks = @user.tasks.page(params[:page])
+    
+    if params[:sort_end_date]
+      @tasks = @user.tasks.order(:end_date).page(params[:page])
+
+    elsif params[:search].present?
+      if task_name_params.present? && status_params.present?        
+        @tasks = @user.tasks.search_task_name(task_name_params).search_status(status_params).page(params[:page])
+
+      elsif task_name_params.present?        
+        @tasks = @user.tasks.search_task_name(task_name_params).page(params[:page])
+
+      elsif status_params.present?
+        @tasks = @user.tasks.search_status(status_params).page(params[:page])
+      else
+        @tasks = @user.tasks.order(created_at: :desc).page(params[:page])
+      end
+      
+    else
+      @tasks = @user.tasks.order(created_at: :desc).page(params[:page])
+      
+    end
 
   end
 
   private
   def user_params
     params.require(:user).permit(:user_name, :email, :password, :password_confirmation)
+  end
+
+  def task_name_params 
+    params[:search][:task_name]
+  end
+
+  def status_params
+    params[:search][:status]
   end
 
 end
