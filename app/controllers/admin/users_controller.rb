bd934e5d5d52fc4c85c3cdfe962f931ab1586931
genki utsumi <zabanya0770@gmail.com>
2023-06-10 20:41:52 +0900

    管理者ユーザー編集画面を実装

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -11,11 +11,26 @@ class Admin::UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to admin_users_path
+    else
+      render :edit
+    end
   end
 
   def show
   end
   
   def destroy
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:user_name, :email, :password, :password_confirmation)
   end
 end
