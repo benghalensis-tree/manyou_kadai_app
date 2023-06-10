62f330d77335cd4aca0b7640957923b66611ec62
genki utsumi <zabanya0770@gmail.com>
2023-06-10 19:40:28 +0900

    他のユーザーが別ユーザーのマイページにアクセスすることを禁止

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
   before_action :login_required
+  before_action :user_limitationed, only:[:show]
 
   private
   def login_required
     redirect_to new_session_path unless current_user
+  end
+
+  def user_limitationed
+    user = User.find(params[:id])
+    redirect_to tasks_path unless current_user == user
   end
 end
