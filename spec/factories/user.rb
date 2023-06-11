2ac0c005eead7f2abdc93c907478de5fa5d0f98f
genki utsumi <zabanya0770@gmail.com>
2023-06-11 18:27:51 +0900

    テストを途中まで実装

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,18 +1,11 @@
 FactoryBot.define do
 
   factory :user do
-    user_name { 'デフォルトのuser_name' }
-    email { 'デフォルトのemail' }
-    password { 111111 }
-    password_confirmation { 111111 }
+    user_name { 'hoge' }
+    email { 'test@gmail.com' }
+    password { '111111' }
+    password_confirmation { '111111' }
     admin { true }
   end
-  
-  factory :second_user do
-    user_name { 'デフォルトのuser_name2' }
-    email { 'デフォルトのemail2' }
-    password { 222222 }
-    password_confirmation { 222222 }
-    admin { false }
-  end
 end
+  
