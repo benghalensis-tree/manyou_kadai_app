192c6f5b32b33d76e6b75f19c721f6174107a974
genki utsumi <zabanya0770@gmail.com>
2023-06-12 15:31:04 +0900

    ラベルをseedにて作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
 # coding: utf-8
 
 User.create(user_name: 'admin', admin: 1, email: "admin@gmail.jp", password: "111111")
+Label.create(label_name: 'テストラベル1')
+Label.create(label_name: 'テストラベル2')
+Label.create(label_name: 'テストラベル3')
+Label.create(label_name: 'テストラベル4')
+Label.create(label_name: 'テストラベル5')
