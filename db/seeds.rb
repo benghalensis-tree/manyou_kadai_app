72742deb9f25cf1219627650543f212b4cab56bf
genki utsumi <zabanya0770@gmail.com>
2023-06-12 22:46:14 +0900

    seedファイルを記述

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,19 @@
 # coding: utf-8
 
-User.create(user_name: 'admin', admin: 1, email: "admin@gmail.jp", password: "111111")
-Label.create(label_name: 'テストラベル1')
-Label.create(label_name: 'テストラベル2')
-Label.create(label_name: 'テストラベル3')
-Label.create(label_name: 'テストラベル4')
-Label.create(label_name: 'テストラベル5')
+User.create(user_name: 'admin', admin: 1, email: "admin@amail.jp", password: "111111")
+
+10.times do |i|
+  user = User.create(user_name: "name#{i}", admin: 0, email: "test#{i}@gmail.jp", password: "111111")
+end
+
+10.times do |i|
+  task = Task.create(task_name: "タスク#{i}", user: user.sample)
+end
+
+10.times do |i|
+  label = Label.create(label_name: "ラベル#{i}")
+end
+
+20.tims do |i|
+  LabelsTask.create(task_id: task.sample, label_id: label.sample)
+end
